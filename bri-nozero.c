94980011f9086ddddb2eb1eab7b77f21a7b48b41
nochar <taglogehadkate@gmail.com>
2023-10-20 21:17:03 +0200

    nozero brightness thing: fix oob read

--- bri-nozero.c
+++ bri-nozero.c
@@ -4,10 +4,13 @@
 
 int main() {
     char buf[BUFSIZ];
+
+    // each time I get something on stdin
     while (fgets(buf, BUFSIZ, stdin)) {
-        char bri_s[7];
+        char bri_s[8] = {0};
         int bri = 0;
 
+        // read brightness
         FILE *bri_file = fopen("/sys/class/backlight/intel_backlight/brightness", "r");
         if (!bri_file) {
             perror("cannot open file");
