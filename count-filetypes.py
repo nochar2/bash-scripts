fbda0c959da1865ee5b9262b2be265b65c268e72
Karel Šebela <karelsebela@protonmail.com>
2025-05-29 13:30:11 +0200

    count-filetypes: add usage, arg support

--- count-filetypes.py
+++ count-filetypes.py
@@ -1,32 +1,49 @@
 #!/usr/bin/env python3
 
 import termplotlib as tpl
-
 import os
 import sys
+
+# NB: this depends on `python-magic`, not `magic`!
 import magic
 from collections import Counter
-mime = magic.Magic(mime=True)
 
+def list_recursively(dir):
+    for root, _, files in os.walk(dir):
+        for file in files:
+            yield os.path.join(root, file)
+        
 def main():
-    mimetypes = {}    
-    
-    for (i, line) in enumerate(sys.stdin):
-        # print(f"Line {i}")
-        line = line.rstrip()
+    if len(sys.argv) != 2:
+        print(f"usage: {sys.argv[0]} DIR/-")
+        print(f"counts all files by their MIME type")
+        print(f"DIR => list all files in DIR")
+        print(f"-   => reads file list from stdin")
 
+        print(f"depends: python-magic, termplotlib")
+        print(f"perf: slow, like 300 files a second")
+        exit(1)
+
+    mimetypes = {}    
+    enumerator = None
+    if sys.argv[1] == "-":
+        iterator = sys.stdin
+    else:
+        iterator = list_recursively(sys.argv[1])
+    
+    for line in iterator:
+        line = line.rstrip()
 
         if not os.path.isfile(line):
             continue
             
-        mimetype = mime.from_file(line)
+        mimetype = magic.from_file(line, mime=True)
 
         if not mimetype in mimetypes:
             mimetypes[mimetype] = 0
         else:
             mimetypes[mimetype] += 1
 
-    print(mimetypes)
 
     fig = tpl.figure()
     types_and_counts = list(mimetypes.items())
